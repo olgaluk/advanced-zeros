094544f60f1a88d2d53613bf62f9d6bc5ad9a499
Olga Luk <olga_ivanova_86@mail.ru>
2019-02-27 01:46:13 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  function getCountNumber(a, b) {
+    let res = 0;
+    while (b > 0) {
+     b = ~~(b/a);
+     res = res + b;
+    }
+    return res;
+  }
+
+  const arr1 = [];
+  const arr2 = [];
+  let x = 2;
+
+  while (base > 1) {
+    while ((base % x) === 0) {
+      base = base/x;
+      arr1.push(x);
+    }
+    x = x + 1;
+  }
+ 
+  for (let i=0; i < arr1.length; i++) {
+    let k = 1;
+    while (arr1[i] === arr1[i+1]) {
+      k = k + 1;
+      i = i + 1;
+    }
+    arr2.push(~~(getCountNumber(arr1[i],number)/k));
+  }
+
+  arr2.sort((a, b) => a - b);
+  return arr2[0];
 }
